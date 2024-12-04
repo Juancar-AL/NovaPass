9cf53df70c16a0eb222c4d7b7cbdc8c0a67dbcce
Sh0y <jc07luengo@gmail.com>
2024-12-05 00:59:17 +0100

    Login

--- test.py
+++ test.py
@@ -1,59 +1,33 @@
 import flet as ft
-import classes_interface as cl
-
-language = "Spanish"
 
 
 def main(page: ft.Page):
-    page.title = "NovaPass | Password Manager"
-    page.theme_mode = ft.ThemeMode.LIGHT
+    page.title = "GridView Example"
+    page.theme_mode = ft.ThemeMode.DARK
+    page.padding = 50
+    page.update()
 
-    def page_change(new_page):
-        page.controls.clear()
-        page.controls.extend(pages.get(new_page, []))
-        page.update()
+    images = ft.GridView(
+        expand=1,
+        runs_count=5,
+        max_extent=150,
+        child_aspect_ratio=1.0,
+        spacing=5,
+        run_spacing=5,
+    )
 
-    # Datos para inputs
-    email_text = "Correo electrónico" if language == "Spanish" else "Email"
-    password_text = "Contraseña" if language == "Spanish" else "Password"
+    page.add(images)
 
-    reg_inputs_data = [
-        {"placeholder_text": "Nombre" if language ==
-            "Spanish" else "Name", "width": 500},
-        {"placeholder_text": email_text, "width": 500},
-        {"placeholder_text": password_text, "password": True,
-            "can_reveal_password": True, "width": 500},
-    ]
-
-    log_inputs_data = [
-        {"placeholder_text": email_text, "width": 500},
-        {"placeholder_text": password_text, "password": True,
-            "can_reveal_password": True, "width": 500},
-    ]
-
-    # Crear instancia de MainInputs para cada página
-    log_page_inputs = cl.MainInputs("Iniciar sesión", log_inputs_data)
-    reg_page_inputs = cl.MainInputs("Registrarse", reg_inputs_data)
-
-    # Instancias de las páginas
-    quit = cl.quit()
-    # Pasar la instancia de MainInputs
-    about_page = cl.AboutPage(page, language, page_change, log_page_inputs)
-    lan = cl.lan_icon(page)
-    # Esta página también necesita la instancia de MainInputs
-    log_page = cl.data_page("Iniciar sesión", log_inputs_data)
-    reg_page = cl.data_page("Registrarse", reg_inputs_data)  # Igual aquí
-
-    # Diccionario de páginas
-    pages = {
-        "welcome": [quit, about_page, lan],
-        "login": [cl.back(page_change, "welcome"), log_page],
-        "register": [cl.back(page_change, "welcome"), reg_page],
-    }
-
-    # Inicializar la página de bienvenida
-    page.controls.extend(pages["welcome"])
+    for i in range(0, 60):
+        images.controls.append(
+            ft.Image(
+                src=f"https://picsum.photos/150/150?{i}",
+                fit=ft.ImageFit.NONE,
+                repeat=ft.ImageRepeat.NO_REPEAT,
+                border_radius=ft.border_radius.all(10),
+            )
+        )
     page.update()
 
 
-ft.app(target=main)
+ft.app(main, view=ft.AppView.WEB_BROWSER)
